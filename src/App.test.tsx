c91a5dd9876c16758a84e0f38e21f5529057fa57
Anton Gluschuk <32397351+AntonGluschuk@users.noreply.github.com>
2020-11-15 11:20:17 +0200

    fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Travis CI/i);
+  const linkElement = screen.getByText(/Heroku/i);
   expect(linkElement).toBeInTheDocument();
 });
