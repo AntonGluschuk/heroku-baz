1d322673a293a684d61f469d21b5e97987ed7d21
Anton Gluschuk <32397351+AntonGluschuk@users.noreply.github.com>
2020-11-16 13:27:11 +0200

    add time 13:26 to App tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div>Hello it's Travis CI and Heroku CD in 13:14</div>
+      <div>Hello it's Travis CI and Heroku CD in 13:26</div>
     </div>
   );
 }
