73e818367e97f157e154fbc27fdb50c284dcc2dd
Anton Gluschuk <32397351+AntonGluschuk@users.noreply.github.com>
2020-11-17 14:15:51 +0200

    change ci-cd master config

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div>Hello it's Travis CI and CD in 12:33</div>
+      <div>Hello it's Travis CI and Heroku CD in 12:33</div>
     </div>
   );
 }
